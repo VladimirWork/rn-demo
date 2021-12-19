8c942c06418166521a68866444f2c90b5568dcae
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2021-12-19 19:38:02 +0300

    use bottom tab navigation
    Signed-off-by: Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>

--- App.js
+++ App.js
@@ -1,116 +1,82 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-import React from 'react';
-import {
-  NativeBaseProvider,
-  Box,
-  Text,
-  Heading,
-  VStack,
-  FormControl,
-  Input,
-  Link,
-  Button,
-  Icon,
-  HStack,
-  Center,
-  Pressable,
-} from 'native-base';
-import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+const Feed = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed!</Text>
+    </View>
+  );
+}
+
+const Profile = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
+
+const Notifications = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications!</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MyTabs = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      activeColor="#e91e63"
+      labelStyle={{ fontSize: 12 }}
+      style={{ backgroundColor: 'tomato' }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarLabel: 'Updates',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 const App = () => {
-  const [selected, setSelected] = React.useState(1);
-
   return (
-    <NativeBaseProvider>
-      <Box flex={1} bg="white" safeAreaTop>
-        <Center flex={1}></Center>
-        <HStack bg="indigo.600" alignItems="center" safeAreaBottom shadow={6}>
-          <Pressable
-            opacity={selected === 0 ? 1 : 0.5}
-            py="3"
-            flex={1}
-            onPress={() => setSelected(0)}>
-            <Center>
-              <Icon
-                mb="1"
-                as={
-                  <MaterialCommunityIcons
-                    name={selected === 0 ? 'home' : 'home-outline'}
-                  />
-                }
-                color="white"
-                size="sm"
-              />
-              <Text color="white" fontSize="14">
-                Home
-              </Text>
-            </Center>
-          </Pressable>
-          <Pressable
-            opacity={selected === 1 ? 1 : 0.5}
-            py="2"
-            flex={1}
-            onPress={() => setSelected(1)}
-          >
-            <Center>
-              <Icon
-                mb="1"
-                as={<MaterialIcons name="search" />}
-                color="white"
-                size="sm"
-              />
-              <Text color="white" fontSize="14">
-                Search
-              </Text>
-            </Center>
-          </Pressable>
-          <Pressable
-            opacity={selected === 2 ? 1 : 0.6}
-            py="2"
-            flex={1}
-            onPress={() => setSelected(2)}
-          >
-            <Center>
-              <Icon
-                mb="1"
-                as={
-                  <MaterialCommunityIcons
-                    name={selected === 2 ? 'cart' : 'cart-outline'}
-                  />
-                }
-                color="white"
-                size="sm"
-              />
-              <Text color="white" fontSize="14">
-                Cart
-              </Text>
-            </Center>
-          </Pressable>
-          <Pressable
-            opacity={selected === 3 ? 1 : 0.5}
-            py="2"
-            flex={1}
-            onPress={() => setSelected(3)}
-          >
-            <Center>
-              <Icon
-                mb="1"
-                as={
-                  <MaterialCommunityIcons
-                    name={selected === 3 ? 'account' : 'account-outline'}
-                  />
-                }
-                color="white"
-                size="sm"
-              />
-              <Text color="white" fontSize="14">
-                Account
-              </Text>
-            </Center>
-          </Pressable>
-        </HStack>
-      </Box>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
 
